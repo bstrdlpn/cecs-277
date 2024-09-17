bd808d15d620e22b0b2ad257237af88f4a3e239c
bstrdlpn <nguyenphanlong@gmail.com>
2024-09-17 16:30:26 -0700

    style: Edit comment to be within 80 char spacing

--- Lab_04/main.py
+++ Lab_04/main.py
@@ -36,20 +36,20 @@ def display_map(map, player):
 def move_player(player, dir, upper_bound):
     """
     moves the player in the selected direction (W=up, A=left, S=down, D=right).
-    The boundaries of the map are between 0-upper_bound. Check that the location
-    that the user is trying to move to isn’t out of bounds. If it isn’t, then
-    update the user’s location by changing the row and column values in the location
-    list by adding or subtracting 1 to the row or column (depending on the direction
-    they moved), otherwise, display an error message and do not update the user’s
-    location.
+    The boundaries of the map are between 0-upper_bound. Check that the location 
+    that the user is trying to move to isn't out of bounds. If it isn't, then 
+    update the user's location by changing the row and column values in the 
+    location list by adding or subtracting 1 to the row or column (depending on 
+    the direction they moved), otherwise, display an error message and do not 
+    update the user's location.
     """
     pass
 
 def count_treasures(map, player, upper_bound):
     """
-    iterates through
-    the surrounding spaces of the user's current location. Keep a count of the number of
-    treasures (T'), and traps ('X') that are in those spaces. Return the two counts.
+    iterates through the surrounding spaces of the user's current location. 
+    Keep a count of the number of treasures (T'), and traps ('X') that are in 
+    those spaces. Return the two counts.
     """
     pass
 
@@ -58,32 +58,32 @@ def main():
     Creates a loop that continues until the player quits. [insert rest of description]
     """
 
-    print("Treasure Hunt!)
+    print("Treasure Hunt!")
     print("Find all 7 treasures without getting caught in a trap. Look around to spot nearby traps and treasures.")
     while True:
         display_map(map, player)
         dir = input("Enter Direction [WASD or L to Look around (hint) or Q to quit]: ").upper()
-            if (len(dir) == 1) and (dir.isalpha()):
-                if dir == ('W' or 'A' or 'S' or 'D'):
-                    move_player(player, dir, upper_bound)
-                    # if movement is invalid:
-                        # print("You cannot move in that direction.")
-                    # if player falls in a trap:
-                        # print("You were caught in a trap!")
-                    
-                    count_treasures_traps(map, player, upper_bound)
-                    # if all treasures were found:
-                        # print("You found all the treasure!")
-                        # break
+        if (len(dir) == 1) and (dir.isalpha()):
+            if dir == ('W' or 'A' or 'S' or 'D'):
+                move_player(player, dir, upper_bound)
+                # if movement is invalid:
+                    # print("You cannot move in that direction.")
+                # if player falls in a trap:
+                    # print("You were caught in a trap!")
                 
-                elif dir == 'L':
-                    "Look around for traps and treasures"
-                
-                elif dir == 'Q':
-                    print("Exiting. Thank you for playing!")
-                    exit()
-            else:
-                print("Invalid input.")
+                count_treasures_traps(map, player, upper_bound)
+                # if all treasures were found:
+                    # print("You found all the treasure!")
+                    # break
+            
+            elif dir == 'L':
+                "Look around for traps and treasures"
+            
+            elif dir == 'Q':
+                print("Exiting. Thank you for playing!")
+                exit()
+        else:
+            print("Invalid input.")
 
     print("Game Over!")
 main()
