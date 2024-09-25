dc768237799c75777201558e99cca552faa3ef95
bstrdlpn <nguyenphanlong@gmail.com>
2024-09-25 11:07:32 -0700

    fix: __lt__ logic not returning correct bool
    if-else logic added that correctly characterizes
    comparison between different tasks. Returns early
    by checking most sig value 'year' -> least sig value
    'desc'

--- Lab_05/task.py
+++ Lab_05/task.py
@@ -1,20 +1,32 @@
 class Task:
     """
-    Attributes:
-    1. description - string description of the task.
-    2. date - due date of the task. A string in the format: MM/DD/YYYY 
-    3. time - time the task is due. A string in the format: HH:MM
+    A task in a planner with due date and time.
 
-    Methods:
-    1. __init__(self, desc, date, time) - assigns the parameters to the attributes.
-    2. get_description(self) - returns the task's description string.
-    3. __str__(self) - returns a string used to display the task's information to 
-        the user in the format: description - Due: date at time.
-    4. __repr__(self) - returns a string used to write the task's information to the 
-        file in the format: task,date,time.
-    5. __lt__(self, other) - returns true if the self task is less than the other 
-        task. Compare by year, then month, then day, then hour, then minute, and 
-        then the task description in alphabetical order.
+    Attributes
+    ----------
+    description  :  str
+        Description of the task.
+    date  :  str
+        Due date of the task in the format MM/DD/YYYY 
+    time  :  str
+        Time the task is due in the format HH:MM
+
+    Methods
+    -------
+    __init__(self, desc, date, time)
+        assigns the parameters to the attributes.
+    get_description(self)
+        returns the task's description string.
+    __str__(self)
+        returns a string used to display the task's information to the user in 
+        the format: description - Due: date at time.
+    __repr__(self)
+        returns a string used to write the task's information to the  file in 
+        the format: task,date,time.
+    __lt__(self, other)
+        returns true if the self task is less than the other task. Compare by 
+        year, then month, then day, then hour, then minute, and then the task 
+        description in alphabetical order.
     """
 
     def __init__(self, desc, date, time):
@@ -27,40 +39,48 @@ class Task:
     def get_description(self):
         """return task description string"""
         return self.desc
-        pass
+
     
     def __str__(self):
         """returns a string used to display the task’s information to the user in the format:
         'desc' - Due: `date` at `time`"""
         return f"{self.desc} - Due: {self.date} at {self.time}"
-        pass
+
     
     def __repr__(self):
-        """returns a string used to write the task’s information to the file in the format: task,date,time"""
-        return self.desc + "," + str(self.date) + "," + str(self.time)
-        pass
-    
+        """returns a string used to write the task's information to the file in the format: 'task','date','time'"""
+        return f"{self.desc},{self.date},{self.time}"
+
+
     def __lt__(self, other):
         """
         return true if the self task is less than the other task.
         compare by year, then month, then day, then hour, then minute, and then the task description in alphabetical order
         """
-        if self.date(year) < other.date(year):
-            return True
-        elif self.date(year) == other.date(year):
-            if self.date(month) < other.date(month):
-                return True
-            elif self.date(month) == other.date(month):
-                if self.date(day) < other.date(day):
-                    return True
-                elif self.date(day) == other.date(day): 
-                    if self.time(hour) < other.time(hour):
-                        return True
-                    elif self.time(hour) == other.time(hour):
-                        if self.time(minute) < other.time(minute):
-                            return True
-                        else:
-                            return False
+        # break MM/DD/YYYY string into month, day, year
+        self_month, self_day, self_year = map(int, self.date.split('/'))
+
+        other_month, other_day, other_year = map(int, other.date.split('/'))
+        
+        # break HH:MM string into hour, minutes
+        self_hour, self_minute = map(int, self.time.split(':'))
+
+        other_hour, other_minute = map(int, other.time.split(':'))
+        
+        # compare by year
+        if self_year != other_year:
+            return self_year < other_year        
+        # compare by month
+        elif self_month != other_month:
+            return self_month < other_month
+        # compare by day
+        elif self_day != other_day:
+            return self_month < other_month
+        # compare by hours
+        elif self_hour != other_hour:
+            return self_hour < other_hour
+        #compare by minutes
+        elif self_minute != other_minute:
+            return self_minute < other_minute
         else:
-            return false
-        pass
+            return self.desc < other.desc
