857a710420b98bc28f5829bd22acb70b544d9c98
bstrdlpn <nguyenphanlong@gmail.com>
2024-10-16 12:12:00 -0700

    fix list construction
    used positional indexing to correctly access the
    vehicle objects and call their 'description_string'
    methods.

--- Lab_08/main.py
+++ Lab_08/main.py
@@ -2,31 +2,38 @@ import check_input
 import car
 import motorcycle
 import truck
+import random
 
 def main():
+
+    # create a 2d list with three racetracks
+    rows, cols = (3, 100)
+    racetracks = [['-' for i in range(cols)] for j in range(rows)]
+    # c_track = "C--------------O------------------------------------------------------------O-----------------------"
+    # mc_track = "M------------------------------O--------------------------------O-----------------------------------"
+    # t_track = "T------------------------O-----------------------------O--------------------------------------------"
+
+
+    # randomly place 2 obstacles into each list
+    elements = 0
+    for row_index, track_row in enumerate(racetracks):
+        while elements != 2:
+            replace_with_zero = random.randint(2, 99)
+            racetracks[row_index][replace_with_zero] = '0'
+            elements += 1
+        elements = 0
+
+    # construct a list of the three vehicle objects
+    racers = [car.Car(), motorcycle.Motorcycle(), truck.Truck()]
     print("Rad Racer!")
     print("Choose a vehicle and race it down the track (player = 'P'). Slow down for obstacles ('O') or else you'll crash!")
-    print("1. Lightning Car - a fast car (6-8 units). Special: Nitro Boost (1.5x speed)")
-    print("2. Swift Bike - a speedy motorcycle (6-8 units). Special: Wheelie (2x speed but there's a chance you'll crash).")
-    print("3. Behemoth Truck - a heavy truck (4-8 units). Special: Ram (2x speed and it smashes through obstacles).")
+    print(f"1. {racers[0].description_string()}")
+    print(f"2. {racers[1].description_string()}")
+    print(f"3. {racers[2].description_string()}")
+    
 
-    c_track = "C--------------O------------------------------------------------------------O-----------------------"
-    mc_track = "M------------------------------O--------------------------------O-----------------------------------"
-    t_track = "T------------------------O-----------------------------O--------------------------------------------"
-
-    player = check_input.get_int_range("Choose your vehicle [1-3]:", 1, 3)
-
-    match player:
-        case 1:
-            print("You chose the Lightning Car!")
-            p_track = c_track.replace("C", "P")
-        case 2:
-            print("You chose the Swift Bike!")
-            p_track = mc_track.replace("M", "P")
-        case 3:
-            print("You chose the Behemoth Truck!")
-            p_track = t_track.replace("T", "P")
-
+    player = check_input.get_int_range("Choose your vehicle (1-3):", 1, 3)
+    """
     print("Lighning Car [Position - 0, Energy - 100]")
     print("Swift Bike [Position - 0, Energy - 100]")
     print("Behemoth Truck [Position - 0, Energy - 100]")
@@ -36,6 +43,7 @@ def main():
         print(p_track + "\n" + c_track + "\n" + t_track)
     elif player == 3:
         print(p_track + "\n" + c_track + "\n" + mc_track)
+    """
 
     # while player position < 100 (or 99(?)):
         # action = check_input.get_int_range("Choose action (1. Fast, 2. Slow, 3. Special Move):", 1, 3)
