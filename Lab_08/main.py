71fa1d62dcc0e7938d0f74e0dca75b86838d34db
bstrdlpn <nguyenphanlong@gmail.com>
2024-10-16 14:45:06 -0700

    change insert_obstacles to insert_elements
    made insert_obstacles function more generic to make
    it applicable for usage in other scenarios. also
    adjusted main() to reflect changes in function usage.

--- Lab_08/main.py
+++ Lab_08/main.py
@@ -18,27 +18,33 @@ def create_tracks(num_racers, track_length):
     tracks = [['-' for i in range(cols)] for j in range(rows)]
     return tracks
 
-def insert_obstacles(tracks, obstacles, start, end):
+def insert_element(tracks, position=None): 
     """
-    Randomly insert 2 obstacles into the racetrack.
+    Randomly inserts '0' elements into the racetrack, OR if provided position data,
+    insert '*' into position.
 
     :param tracks: list type; racetrack(s) to insert obstacles into
-    :param start: int type; start index
-    :param end: int type; end index
+    :param position: tuple type; position of element to replace, OPTIONAL
     
-    :returns: list of tracks with obstacles inserted.
+    :returns: list of tracks with elements inserted
     """
-   
-    inserted_obstacles = 0
-    # outer loop indexes into the racetrack list
-    for row_index, track in enumerate(tracks):
-        # inner loop replaces a rand element with a zero
-        # after reqwuired number of obstacles inserted, break inner loop
-        while inserted_obstacles != 2:
-            replace_with_zero = random.randint(start + 1, end - 1)
-            tracks[row_index][replace_with_zero] = '0'
-            inserted_obstacles += 1
+    # if optional parameters none (we are creating the tracks):
+    if position is None:
         inserted_obstacles = 0
+        # outer loop indexes into the racetrack list
+        for row_index, track in enumerate(tracks):
+            # inner loop replaces a rand element with a zero
+            # after reqwuired number of obstacles inserted, break inner loop
+            while inserted_obstacles != 2:
+                # magic constants for now, haven't figured out how to parametrize
+                replace_with_zero = random.randint(2, 99)
+                tracks[row_index][replace_with_zero] = '0'
+                inserted_obstacles += 1
+            inserted_obstacles = 0
+    # if the optional has a value, replace with *
+    else:
+        row, col = position
+        tracks[row][col] = '*'
 
     return tracks
 
@@ -64,14 +70,19 @@ def print_state_take_action(vehicles, tracks):
 
 def main():
 
+    # win placement
+    win_place = []
+
     # create a 2d list with three racetracks
     racetracks = create_tracks(3, 100)
     # c_track = "C--------------O------------------------------------------------------------O-----------------------"
     # mc_track = "M------------------------------O--------------------------------O-----------------------------------"
     # t_track = "T------------------------O-----------------------------O--------------------------------------------"
 
+
     # randomly place 2 obstacles into each list
-    racetracks = insert_obstacles(racetracks, 2, 0, 99)
+    racetracks = insert_element(racetracks)
+
 
     # construct a list of the three vehicle objects
     racers = [car.Car(), motorcycle.Motorcycle(), truck.Truck()]
@@ -102,13 +113,21 @@ def main():
             pass
         
     choice = print_state_take_action(racers, racetracks)
-    # while player position < 100 (or 99(?)):
-        # action = check_input.get_int_range("Choose action (1. Fast, 2. Slow, 3. Special Move):", 1, 3)
-        # move player appropriate distance
-        # move opponents
-        # if player hits an obstacle:
-            # print(user hit an obstacle and crashed)
-        # print out opponent status/movements
+    
+    # while all vehicles < 99
+    while racers[0].position or racers[1].position or racers[2].position < 99:
+        pass
+        # print the state and take action
+
+        # move player appropriate distance AND save previous position (to update map with *)
+
+        # move opponents AND save previous position (to update map with *)
+
+        # print player state/moves
+        # print out opponent status/moves
+
+        # IF vehicle has reached end, assign win placement (maybe list?)
+        
         # update map
 
 
