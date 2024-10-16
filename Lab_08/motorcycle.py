0321639c8a278d207bdd3d7807ef4ad209ce4498
bstrdlpn <nguyenphanlong@gmail.com>
2024-10-15 18:05:24 -0700

    add logic for class functions
    add prelim logic for the class functions. requires
    testiing.

--- Lab_08/motorcycle.py
+++ Lab_08/motorcycle.py
@@ -1,4 +1,5 @@
 import vehicle
+import random
 
 class Motorcycle(vehicle.Vehicle):
     def __init__(self, name, initial, min_speed, max_speed):
@@ -10,17 +11,26 @@ class Motorcycle(vehicle.Vehicle):
         self.min_speed = 6
         self.max_speed = 8
 
+
     def slow(self, dist):
         """overridden method - passes in distance to the next obstacle (if there 
         is one). The motorcycle will move at 75% speed, rather than half. If 
         there's an obstacle, then it will go around it. There is no energy cost. 
         Return a string that describes the event that occurred with the name of 
         the motorcycle and the distance traveled (if applicable)"""
-        pass
+        move = round(random.randrange(self.min_speed, self.max_speed) * .75)
+
+        if move + self._position > dist + self._position:
+            self._position += move
+            return f"{self._name} slowly and safely moves around the obstacle {move} units!"
+        else:
+            self._position += move
+            return f"{self._name} slowly moves {move} units!"
+
 
     def description_string(self):
         """returns a string with the motorcycles's stats and abilities"""
-        pass
+        return f"{self._name} - a speedy motorcycle ({self.min_speed}-{self.max_speed}). Special: Wheelie (2x speed but there's a chance you'll crash)."
 
     def special_move(self, dist):
         """passes in distance to the next obstacle (if there is one). If there 
@@ -31,4 +41,22 @@ class Motorcycle(vehicle.Vehicle):
         otherwise it moves the randomized distance. Return a string that 
         describes the event that occurred with the name of the motorcycle and 
         the distance traveled (if applicable)"""
-        pass
+
+        chance = random.randint(1, 4)
+        
+        # if roll on chance is 4 - fail state
+        if self._energy >= 15 and chance == 4:
+            self._position += 1
+            return f"{self._name} pops a wheelie and falls over!"
+        # if roll is less than 4
+        if self._energy >= 15 and chance < 4:
+            move = random.randrange({self.min_speed}, {self.max_speed}) * 2
+            # if there is an obstacle in the way
+            if move + self._position > dist + self._position:
+                self._position += dist - 1
+                return f"{self._name} pops a wheelie and crashes into an obstacle!"
+            # if there is no obstacle
+            else:
+                self._position += move
+                return f"{self._name} pops a wheelie and moves {move} units!"
+
