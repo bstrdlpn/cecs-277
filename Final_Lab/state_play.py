5260c7cacf281e61854b397c94914999c446d176
bstrdlpn <nguyenphanlong@gmail.com>
2024-12-03 22:54:54 -0800

    edited docstring style, imperative mood

--- Final_Lab/state_play.py
+++ Final_Lab/state_play.py
@@ -1,10 +1,27 @@
 import puppy_state
 import state_asleep
-import state_eat
 
 class StatePlay(puppy_state.PuppyState):
     def feed(self, puppy):
-        pass
+        """
+        Attempt to feed the puppy.
+
+        :return: str; description of feed attempt failure.
+        """
+        return "The puppy is too busy playing with the ball to eat right now."
 
     def play(self, puppy):
-        pass
+        """
+        Play with the puppy, if puppy has been played with consecutively 3 times, 
+        change state to 'StateAsleep' and return string of what puppy does, else 
+        return string desc of what puppy does.
+
+        :return: str; description of what puppy does
+        """
+        puppy.inc_plays()
+        if puppy.plays % 3 == 0:
+            puppy.reset()
+            puppy.change_state(state_asleep.StateAsleep())
+            return "The puppy played so much it fell asleep!"
+        else:
+            return "You throw the ball again and the puppy excitedly chases it."
