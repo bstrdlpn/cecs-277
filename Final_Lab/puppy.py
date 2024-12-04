798e05957e8a9dfef7566a5ab9c4184cf1b1b48c
bstrdlpn <nguyenphanlong@gmail.com>
2024-12-03 22:48:16 -0800

    style formatting for docstrings, changed to imperative.

--- Final_Lab/puppy.py
+++ Final_Lab/puppy.py
@@ -1,12 +1,15 @@
-import puppy_state
 import state_asleep
 
-class Puppy(puppy_state.PuppyState):
+class Puppy:
+    """
+    Represents the user's puppy. The user can play or feed the puppo.
+    """
     def __init__(self):
         """
-        initializes the state to the asleep state, and then initializes the number of feeds and plays
+        Initialize the state to the asleep state, and then initialize the number 
+        of feeds and plays
         """
-        self._state = state_asleep.StateAsleep
+        self._state = state_asleep.StateAsleep()
         self._feeds = 0
         self._plays = 0
 
@@ -22,39 +25,37 @@ class Puppy(puppy_state.PuppyState):
 
     def change_state(self, new_state):
         """
-        updates the puppy's state to the new state
+        Update the puppy's state to the new state.
         """
         self._state = new_state
 
     def throw_ball(self):
         """
-        calls the play method for whichever state the puppy is in
+        Call the play method for whichever state the puppy is in.
         """
-        play = self.play()
-        return play
+        return self._state.play(self)
 
     def give_food(self):
         """
-        calls the feed method for whichever state the puppy is in
+        Call the feed method for whichever state the puppy is in.
         """
-        feed = self.feed()
-        return feed
+        return self._state.feed(self)
 
     def inc_feeds(self):
         """
-        increments the number of times the puppy has been fed in a row
+        Increment the number of times the puppy has been fed in a row.
         """
         self._feeds += 1
 
     def inc_plays(self):
         """
-        increments the number of times the puppy has played in a row.
+        Increments the number of times the puppy has played in a row.
         """
         self._plays += 1
 
     def reset(self):
         """
-        reinitializes the feeds and plays attributes
+        Reinitialize the feeds and plays attributes.
         """
         self._feeds = 0
         self._plays = 0
